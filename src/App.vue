f499e1e1450186e4d200f37e92d3d37abaf2f380
SeanErvinson <ervinsonong@gmail.com>
2019-10-07 19:55:22 +0800

    Removed duplicate style

--- src/App.vue
+++ src/App.vue
@@ -14,49 +14,4 @@ export default {
 </script>
 
 <style>
-.commands {
-  background-color: #f9f6f2;
-  border-radius: 3px;
-  color: #132743;
-  padding: 2px;
-}
-.commands li {
-  display: inline;
-}
-.command {
-  padding: 4px;
-}
-.command:hover {
-  color: #224372;
-}
-.textarea {
-  border: none;
-  background-color: white;
-  border-radius: 3px;
-  min-height: 24em;
-  height: 100%;
-  width: 100%;
-  resize: none;
-  padding: 8px;
-  font-size: 1.1em;
-  font-family: "Open Sans", sans-serif;
-}
-.info {
-  background-color: #8ba8b7;
-}
-.details li {
-  list-style-type: square;
-}
-button {
-  color: white;
-  background-color: transparent;
-  border: 2px solid white;
-  border-radius: 5px;
-  text-align: center;
-  padding: 4px;
-  width: 100%;
-}
-button:hover {
-  background-color: #8ba8b7;
-}
 </style>
